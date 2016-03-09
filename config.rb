795d83ae81625eb39c80e1e69eb754af5183851d
Dan Halliday <dan@danhalliday.com>
2016-03-09 21:13:01 +0000

    New contact icon; tweaked copy; added to programming skills list

--- config.rb
+++ config.rb
@@ -1,34 +1,4 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
 proxy "/googlecfca002f899d9032.html", "/verify.html", { layout: nil, ignore: true, }
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ignore '*.psd'
 ignore '*.sketch'
@@ -36,11 +6,11 @@ ignore '*.nef'
 
 ignore 'images/screenshots/*'
 
-###
-# Helpers
-###
-
 activate :autoprefixer
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
 
 activate :sync do |sync|
   Fog.credentials = { :path_style => true }
@@ -54,35 +24,18 @@ activate :sync do |sync|
   sync.after_build = false
 end
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-
-# Build-specific configuration
 configure :build do
   activate :minify_css, inline: true
   activate :minify_javascript, inline: true
+  activate :asset_hash
+  activate :relative_assets
   activate :gzip
 
   activate :minify_html do |html|
     html.remove_intertag_spaces = true
   end
-
-  activate :asset_hash
-  activate :relative_assets
 end
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# Portfolio Website
+# My Personal Website
 
 [![Build Status](https://travis-ci.org/danhalliday/danhalliday.com.svg?branch=master)](https://travis-ci.org/danhalliday/danhalliday.com)
